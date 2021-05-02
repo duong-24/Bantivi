17cb33e420912bb7ed65c479b608efd695969828
duong-24 <69850980+duong-24@users.noreply.github.com>
2021-05-02 10:41:38 +0700

    5/2/2021

---
+++ js/js1.js
@@ -0,0 +1,9 @@
+var i=1;
+function buttononclick(){
+    i++;
+    document.getElementById('values1').value=i;
+}
+function buttononclick2(){
+    i--;
+    document.getElementById('values1').value=i;
+}
